06cdc40a13b415b5106f2b389e86ea4d6ff10caa
bpc <wpesfriendnva@gmail.com>
2024-03-25 14:11:03 -0400

    Forgot to add hash.c

---
+++ src/hash.c
@@ -0,0 +1,11 @@
+#include "bed.h"
+
+unsigned long hash(char **buf)
+{
+  unsigned long hash = 5381;
+  for(int i = 0; buf[i]; i++) {
+    for(int j = 0; buf[i][j]; j++)
+      hash = ((hash << 5) + hash) + buf[i][j];
+  }
+  return hash;
+}
